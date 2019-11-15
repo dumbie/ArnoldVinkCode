4b972396add6e54afe64644bd2bca6be37ca4aca
Arnold Vink <info@arnoldvink.com>
2019-11-15 18:08:16 +0100

    - Moved some reused interface code to ArnoldVinkCode.

---
+++ Functions/Desktop/AVInterface.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using static LibraryShared.Classes;
+using static LibraryShared.OutputKeyboard;
+
+namespace ArnoldVinkCode
+{
+    public partial class AVInterface
+    {
+        //Force focus on a framework element
+        public static async Task FocusOnElement(FrameworkElement focusElement, bool mouseCapture, IntPtr mainWindowHandle)
+        {
+            try
+            {
+                if (focusElement != null && focusElement.Focusable && focusElement.Visibility == Visibility.Visible)
+                {
+                    int WhileLoop = 0;
+                    while (Keyboard.FocusedElement != focusElement)
+                    {
+                        //Update the element layout
+                        focusElement.UpdateLayout();
+
+                        //Logical focus on the element
+                        focusElement.Focus();
+
+                        //Keyboard focus on the element
+                        Keyboard.Focus(focusElement);
+
+                        //Mouse capture the element
+                        if (mouseCapture)
+                        {
+                            Mouse.Capture(focusElement);
+                        }
+
+                        if (WhileLoop >= 30)
+                        {
+                            Debug.WriteLine("Failed focusing on the element after " + WhileLoop + " times, pressing tab key.");
+                            KeySendSingle((byte)KeysVirtual.Tab, mainWindowHandle);
+                            return;
+                        }
+
+                        WhileLoop++;
+                        await Task.Delay(10);
+                    }
+
+                    //Debug.WriteLine("Forced keyboard focus on: " + focusElement);
+                }
+                else
+                {
+                    Debug.WriteLine("Focus element cannot be focused on, pressing tab key.");
+                    KeySendSingle((byte)KeysVirtual.Tab, mainWindowHandle);
+                }
+            }
+            catch
+            {
+                Debug.WriteLine("Failed focusing on the element, pressing tab key.");
+                KeySendSingle((byte)KeysVirtual.Tab, mainWindowHandle);
+            }
+        }
+    }
+}
