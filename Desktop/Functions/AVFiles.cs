91884ef6af121b3d2187ff5dbc4095a37eb1c735
Arnold Vink <info@arnoldvink.com>
2020-01-25 00:05:58 +0100

    - Added AVFiles to Desktop with basic features like renaming and removing.

---
+++ Desktop/Functions/AVFiles.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace ArnoldVinkCode
+{
+    public partial class AVFiles
+    {
+        //Directory remove
+        public static void Directory_Remove(string removeDirectoryPath)
+        {
+            try
+            {
+                if (Directory.Exists(removeDirectoryPath))
+                {
+                    Debug.WriteLine("Removing: " + removeDirectoryPath);
+                    Directory.Delete(removeDirectoryPath, true);
+                }
+            }
+            catch
+            {
+                Debug.WriteLine("Failed removing directory: " + removeDirectoryPath);
+            }
+        }
+
+        //File remove
+        public static void File_Remove(string removeFilePath)
+        {
+            try
+            {
+                if (File.Exists(removeFilePath))
+                {
+                    Debug.WriteLine("Removing: " + removeFilePath);
+                    File.Delete(removeFilePath);
+                }
+            }
+            catch
+            {
+                Debug.WriteLine("Failed removing file: " + removeFilePath);
+            }
+        }
+
+        //File rename
+        public static void File_Rename(string oldFilePath, string newFilePath, bool overWrite)
+        {
+            try
+            {
+                if (File.Exists(oldFilePath))
+                {
+                    Debug.WriteLine("Renaming: " + oldFilePath + " to " + newFilePath);
+                    if (overWrite) { File_Remove(newFilePath); }
+                    File.Move(oldFilePath, newFilePath);
+                }
+            }
+            catch
+            {
+                Debug.WriteLine("Failed renaming file: " + oldFilePath + " to" + newFilePath);
+            }
+        }
+    }
+}
