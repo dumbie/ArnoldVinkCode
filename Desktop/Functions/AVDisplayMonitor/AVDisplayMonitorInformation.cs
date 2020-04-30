6433197c927fb9380557bfde10fcbc13ee52ac2b
Arnold Vink <info@arnoldvink.com>
2020-04-30 19:29:49 +0200

    - Added code to read monitor information like the current refresh rate.

---
+++ Desktop/Functions/AVDisplayMonitor/AVDisplayMonitorInformation.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace ArnoldVinkCode
+{
+    public partial class AVDisplayMonitor
+    {
+        public static int GetScreenRefreshRate(Screen targetScreen)
+        {
+            try
+            {
+                DEVMODE devMode = new DEVMODE();
+                EnumDisplaySettings(targetScreen.DeviceName, IMODENUM.ENUM_CURRENT_SETTINGS, ref devMode);
+                //Debug.WriteLine("Monitor refresh rate: " + devMode.dmDisplayFrequency + "Hz");
+                return devMode.dmDisplayFrequency;
+            }
+            catch { }
+            return 0;
+        }
+    }
+}
