a6f1215315c85983d05a4c24be56ba6a56c33881
Arnold Vink <info@arnoldvink.com>
2020-11-26 17:03:49 +0100

    - Added basic GitHub api code. - Fixed ComboBox drop down width.

---
+++ Desktop/Api/GitHub/GitHub_Paths.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ArnoldVinkCode
+{
+    public partial class ApiGitHub
+    {
+        public static string ApiGitHub_GetDownloadPath(string userName, string projectName, string fileName)
+        {
+            try
+            {
+                return "https://github.com/" + userName + "/" + projectName + "/releases/latest/download/" + fileName;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed to get GitHub download path: " + ex.Message);
+                return string.Empty;
+            }
+        }
+    }
+}
--- Desktop/Api/GitHub/GitHub_Version.cs
+++ Desktop/Api/GitHub/GitHub_Version.cs
@@ -7,19 +7,6 @@ namespace ArnoldVinkCode
 {
     public partial class ApiGitHub
     {
-        public static string ApiGitHub_GetDownloadPath(string userName, string projectName, string fileName)
-        {
-            try
-            {
-                return "https://github.com/" + userName + "/" + projectName + "/releases/latest/download/" + fileName;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("Failed to get GitHub download path: " + ex.Message);
-                return string.Empty;
-            }
-        }
-
         public static async Task<string> ApiGitHub_GetLatestVersion(string userName, string projectName)
         {
             try
