68d1d317acec13ae4c6532d01ed8565435f44b84
Arnold Vink <info@arnoldvink.com>
2021-08-05 10:44:19 +0200

    - Added SetThreadExecutionState to interop to prevent display sleeping.

--- Desktop/Interop/AVInteropDll.cs
+++ Desktop/Interop/AVInteropDll.cs
@@ -681,5 +681,17 @@ namespace ArnoldVinkCode
 
         [DllImport("kernel32.dll")]
         public static extern uint WaitForSingleObject(IntPtr hEvent, uint dwMilliseconds);
+
+        //Prevent sleep or monitor timeout
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+        }
     }
 }
