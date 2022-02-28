5e5811265d8e1a8a7766c99e44cd6fec5c10ca7e
Arnold Vink <info@arnoldvink.com>
2022-02-28 11:02:55 +0100

    - Added simple code to write debug text to file.

---
+++ Desktop/Functions/AVDebug.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ArnoldVinkCode
+{
+    public partial class AVDebug
+    {
+        //Write debug text to file
+        public static bool WriteFile(string debugText)
+        {
+            try
+            {
+                if (!debugText.EndsWith("\n") && !debugText.EndsWith("\r\n"))
+                {
+                    File.AppendAllText("AVDebug.txt", debugText + "\r\n");
+                }
+                else
+                {
+                    File.AppendAllText("AVDebug.txt", debugText);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed appending debug text: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
