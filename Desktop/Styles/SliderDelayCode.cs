d57bd420ee08547ab740493b5f230a717466ec90
Arnold Vink <info@arnoldvink.com>
2023-01-23 11:33:02 +0100

    - Added SliderDelay and TextBoxDelay that delays value changes.

---
+++ Desktop/Styles/SliderDelayCode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace ArnoldVinkCode.Styles
+{
+    //Import:
+    //xmlns:AVStyles="clr-namespace:ArnoldVinkCode.Styles;assembly=ArnoldVinkCode"
+    //Usage:
+    //<AVStyles:SliderDelay/>
+
+    public class SliderDelay : Slider
+    {
+        private bool SliderThumbDragging = false;
+        private DispatcherTimer DispatcherTimerDelay = new DispatcherTimer();
+
+        protected override void OnThumbDragCompleted(System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            SliderThumbDragging = false;
+            base.OnThumbDragCompleted(e);
+        }
+
+        protected override void OnThumbDragStarted(System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            SliderThumbDragging = true;
+            base.OnThumbDragStarted(e);
+        }
+
+        protected override void OnValueChanged(double oldValue, double newValue)
+        {
+            AVFunctions.TimerRenew(ref DispatcherTimerDelay);
+            DispatcherTimerDelay.Interval = TimeSpan.FromMilliseconds(250);
+            DispatcherTimerDelay.Tick += delegate
+            {
+                if (!SliderThumbDragging)
+                {
+                    //Debug.WriteLine("Slider value change delayed.");
+                    DispatcherTimerDelay.Stop();
+                    base.OnValueChanged(oldValue, newValue);
+                }
+            };
+            DispatcherTimerDelay.Start();
+        }
+    }
+}
---
+++ Desktop/Styles/TextBoxDelayCode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace ArnoldVinkCode.Styles
+{
+    //Import:
+    //xmlns:AVStyles="clr-namespace:ArnoldVinkCode.Styles;assembly=ArnoldVinkCode"
+    //Usage:
+    //<AVStyles:TextBoxDelay/>
+
+    public class TextBoxDelay : TextBox
+    {
+        private DispatcherTimer DispatcherTimerDelay = new DispatcherTimer();
+
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            AVFunctions.TimerRenew(ref DispatcherTimerDelay);
+            DispatcherTimerDelay.Interval = TimeSpan.FromMilliseconds(1000);
+            DispatcherTimerDelay.Tick += delegate
+            {
+                //Debug.WriteLine("Textbox text change delayed.");
+                DispatcherTimerDelay.Stop();
+                base.OnTextChanged(e);
+            };
+            DispatcherTimerDelay.Start();
+        }
+    }
+}
