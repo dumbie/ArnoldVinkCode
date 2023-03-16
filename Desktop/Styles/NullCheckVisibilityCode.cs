ef7d5754e2a42af4c436ae0277ee53b4cc191a9f
Arnold Vink <info@arnoldvink.com>
2023-03-16 12:20:33 +0100

    - Added code to check if binding is visible or collapsed.

---
+++ Desktop/Styles/NullCheckVisibilityCode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ArnoldVinkCode.Styles
+{
+    //Import:
+    //xmlns:styles="clr-namespace:ArnoldVinkCode.Styles"
+    //Usage:
+    //Visibility="{Binding Name, Converter={styles:CheckVisibility}}"/>
+
+    //Check if binding is visible or collapsed
+    public class CheckVisibility : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value == null)
+                {
+                    return Visibility.Collapsed;
+                }
+
+                Type valueType = value.GetType();
+                if (valueType == typeof(string))
+                {
+                    return string.IsNullOrWhiteSpace((string)value) ? Visibility.Collapsed : Visibility.Visible;
+                }
+
+                if (valueType == typeof(bool))
+                {
+                    return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+                }
+            }
+            catch { }
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
