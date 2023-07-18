3b4d781f69d78aaff197bae0a400a5fcbbe40eae
Arnold Vink <info@arnoldvink.com>
2023-07-18 10:43:29 +0200

    - Added ComboBoxItemValue class.

---
+++ Desktop/Classes/ComboBoxItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ArnoldVinkCode
+{
+    public static class AVClasses
+    {
+        public class ComboBoxItemValue
+        {
+            public string Text { get; set; }
+            public string Value { get; set; }
+        }
+    }
+}
