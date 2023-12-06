570fc6c88e26c8900d193a4ef1715d4474993bce
Arnold Vink <vink_arnold@hotmail.com>
2023-12-06 13:27:16 +0100

    - Added task to restart keyboard hook on window change.

--- Desktop/Functions/ArnoldVinkInputOutput/InputOutputHotKey.cs
+++ Desktop/Functions/ArnoldVinkInputOutput/InputOutputHotKey.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using static ArnoldVinkCode.AVActions;
 using static ArnoldVinkCode.AVInputOutputClass;
 using static ArnoldVinkCode.AVInteropDll;
 
@@ -22,23 +24,119 @@ namespace ArnoldVinkCode
         public delegate void HotKeyPressed(List<KeysVirtual> keysPressed);
         public static event HotKeyPressed EventHotKeyPressed;
 
+        //Tasks
+        private static AVTaskDetails vTask_RestartKeyboardHook = new AVTaskDetails("vTask_RestartKeyboardHook");
+
         //Start receiving key input
-        public static void Start()
+        public static bool Start()
+        {
+            bool hooked = false;
+            try
+            {
+                //Set window keyboard hook
+                hooked = WindowKeyboardHook();
+
+                //Start task detecting window change
+                if (hooked)
+                {
+                    TaskStartLoop(vTaskLoop_RestartKeyboardHook, vTask_RestartKeyboardHook);
+                }
+            }
+            catch { }
+            return hooked;
+        }
+
+        //Stop receiving key input
+        public static async Task Stop()
         {
             try
             {
-                vWindowHookPointer = SetWindowsHookEx(WindowHookTypes.WH_KEYBOARD_LL, vLowLevelKeyboardCallback, IntPtr.Zero, 0);
+                //Remove window keyboard hook
+                WindowKeyboardUnhook();
+
+                //Stop task detecting window change
+                await TaskStopLoop(vTask_RestartKeyboardHook, 5000);
             }
             catch { }
         }
 
-        //Stop receiving key input
-        public static void Stop()
+        //Restart receiving key input
+        public static bool Restart()
+        {
+            bool hooked = false;
+            try
+            {
+                WindowKeyboardUnhook();
+                hooked = WindowKeyboardHook();
+            }
+            catch { }
+            return hooked;
+        }
+
+        //Set window keyboard hook
+        private static bool WindowKeyboardHook()
         {
             try
             {
-                UnhookWindowsHookEx(vWindowHookPointer);
-                vWindowHookPointer = IntPtr.Zero;
+                if (vWindowHookPointer != IntPtr.Zero)
+                {
+                    Debug.WriteLine("Window keyboard hook already set, unhook first.");
+                    return false;
+                }
+
+                AVActions.DispatcherInvoke(delegate
+                {
+                    vWindowHookPointer = SetWindowsHookEx(WindowHookTypes.WH_KEYBOARD_LL, vLowLevelKeyboardCallback, IntPtr.Zero, 0);
+                    Debug.WriteLine("Hooked window keyboard: " + vWindowHookPointer);
+                });
+            }
+            catch { }
+            return vWindowHookPointer != IntPtr.Zero;
+        }
+
+        //Remove window keyboard hook
+        private static void WindowKeyboardUnhook()
+        {
+            try
+            {
+                if (vWindowHookPointer != IntPtr.Zero)
+                {
+                    AVActions.DispatcherInvoke(delegate
+                    {
+                        bool unhooked = UnhookWindowsHookEx(vWindowHookPointer);
+                        Debug.WriteLine("Unhooked window keyboard: " + unhooked);
+                    });
+                    vWindowHookPointer = IntPtr.Zero;
+                }
+            }
+            catch { }
+        }
+
+        //Task detecting window change
+        private static async Task vTaskLoop_RestartKeyboardHook()
+        {
+            try
+            {
+                //Loop variables
+                IntPtr previousForegroundWindow = GetForegroundWindow();
+
+                while (TaskCheckLoop(vTask_RestartKeyboardHook))
+                {
+                    //Get foreground window
+                    IntPtr currentForegroundWindow = GetForegroundWindow();
+
+                    //Check foreground window change
+                    if (previousForegroundWindow != currentForegroundWindow)
+                    {
+                        Restart();
+                    }
+
+                    //Update previous variables
+                    previousForegroundWindow = currentForegroundWindow;
+
+                    //Delay the loop task
+                    await TaskDelay(1000, vTask_RestartKeyboardHook);
+                }
             }
             catch { }
         }
