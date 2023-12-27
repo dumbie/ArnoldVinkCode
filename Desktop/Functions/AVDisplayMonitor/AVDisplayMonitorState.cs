cb83b0a7bee3cc54a20145c21b65b914a4275cb2
Arnold Vink <vink_arnold@hotmail.com>
2023-12-27 16:34:26 +0100

    - Added code to sleep monitor.

---
+++ Desktop/Functions/AVDisplayMonitor/AVDisplayMonitorState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using static ArnoldVinkCode.AVInteropDll;
+
+namespace ArnoldVinkCode
+{
+    public partial class AVDisplayMonitor
+    {
+        public static bool MonitorStateSetSleep()
+        {
+            try
+            {
+                uint MONITOR_STATE = 2;
+                int SC_MONITORPOWER = 0xF170;
+                IntPtr HWND_BROADCAST = new IntPtr(0xFFFF);
+                IntPtr result = SendMessage(HWND_BROADCAST, WindowMessages.WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_STATE);
+                Debug.WriteLine("Set display monitor state to sleep: " + result);
+            }
+            catch { }
+            return false;
+        }
+    }
+}
