b832fadc3ef0b93288d9964f36ed03ef63433026
Arnold Vink <vink_arnold@hotmail.com>
2024-04-04 11:58:18 +0200

    - Added fix for UWP await not working in .NET core.

---
+++ Desktop/Helpers/AVUwpCore/AVUwpAwait.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Windows.Foundation;
+
+public static class AVUwpAwait
+{
+    public static TaskAwaiter GetAwaiter(this IAsyncAction source)
+    {
+        return source.AsTask().GetAwaiter();
+    }
+
+    public static TaskAwaiter GetAwaiter<TProgress>(this IAsyncActionWithProgress<TProgress> source)
+    {
+        return source.AsTask().GetAwaiter();
+    }
+
+    public static TaskAwaiter<TResult> GetAwaiter<TResult>(this IAsyncOperation<TResult> source)
+    {
+        return source.AsTask().GetAwaiter();
+    }
+
+    public static TaskAwaiter<TResult> GetAwaiter<TResult, TProgress>(this IAsyncOperationWithProgress<TResult, TProgress> source)
+    {
+        return source.AsTask().GetAwaiter();
+    }
+}
---
+++ Desktop/Helpers/AVUwpCore/AVUwpStream.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Windows.Storage.Streams;
+
+public static class AVUwpStream
+{
+    public static Stream AsStream(this IRandomAccessStream windowsRuntimeStream)
+    {
+        try
+        {
+            return windowsRuntimeStream.AsStreamForWrite();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("Failed to convert UWP random access stream: " + ex.Message);
+            return null;
+        }
+    }
+}
