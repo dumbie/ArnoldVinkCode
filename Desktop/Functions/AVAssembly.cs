dcc0494fb314ae04d6b4e3b4dd399fd59e7fd271
Arnold Vink <vink_arnold@hotmail.com>
2024-04-04 11:26:09 +0200

    - Added code to resolve missing assembly dll files. - Added code to convert embedded file to bytes.

---
+++ Desktop/Functions/AVAssembly.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using static ArnoldVinkCode.AVEmbedded;
+
+namespace ArnoldVinkCode
+{
+    public partial class AVAssembly
+    {
+        public static Assembly AppAssemblyResolveFile(object sender, ResolveEventArgs args)
+        {
+            try
+            {
+                string fileName = args.Name.Split(',')[0] + ".dll";
+                string filePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\Resources\\" + fileName;
+                Debug.WriteLine("Resolving resource assembly dll: " + fileName);
+                return Assembly.LoadFrom(filePath);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed resolving assembly dll: " + ex.Message);
+                return null;
+            }
+        }
+
+        public static Assembly AppAssemblyResolveEmbedded(object sender, ResolveEventArgs args)
+        {
+            try
+            {
+                string fileName = args.Name.Split(',')[0] + ".dll";
+                string assemblyPath = Assembly.GetEntryAssembly().GetName().Name + ".Assembly." + fileName;
+                byte[] fileBytes = EmbeddedResourceToBytes(assemblyPath);
+                Debug.WriteLine("Resolving embedded assembly dll: " + assemblyPath);
+                return Assembly.Load(fileBytes);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed resolving assembly dll: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ Desktop/Functions/AVEmbedded.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace ArnoldVinkCode
+{
+    public partial class AVEmbedded
+    {
+        public static byte[] EmbeddedResourceToBytes(string fileName)
+        {
+            try
+            {
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (Stream fileStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(fileName))
+                    {
+                        fileStream.CopyTo(memoryStream);
+                        return memoryStream.ToArray();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed to convert embedded resource: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
