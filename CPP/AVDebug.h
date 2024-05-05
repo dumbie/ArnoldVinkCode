eb39f9a4766a356b9d17f21e60a806bf298b3944
Arnold Vink <vink_arnold@hotmail.com>
2024-05-05 20:53:14 +0200

    - Added C++ Debug.WriteLine alternative.

--- CPP/AVDebug.h
+++ CPP/AVDebug.h
@@ -1,20 +1,23 @@
 #pragma once
 #include <sstream>
+#define AVDebugWriteLine(message) (AVDebugWriteLineInternal() << message)
 
-class AVDebugWriteLine
+//Usage example: AVDebugWriteLine("Hello " << "World " << 1);
+//Usage example: AVDebugWriteLineInternal() << "Hello " << "World " << 1;
+class AVDebugWriteLineInternal
 {
 private:
 	std::ostringstream stringstream;
 
 public:
 	template <class T>
-	AVDebugWriteLine& operator << (const T& value)
+	AVDebugWriteLineInternal& operator << (const T& value)
 	{
 		stringstream << value;
 		return *this;
 	}
 
-	~AVDebugWriteLine()
+	~AVDebugWriteLineInternal()
 	{
 		stringstream << "\n";
 		std::cout << stringstream.str() << std::flush;
