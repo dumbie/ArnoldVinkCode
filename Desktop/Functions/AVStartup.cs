ceb9f5e5e234972a09fe01dea928ffd3a6d5d8d9
Arnold Vink <vink_arnold@hotmail.com>
2024-05-13 16:04:57 +0200

    - Added assembly dll resolving to startup code.

--- Desktop/Functions/AVStartup.cs
+++ Desktop/Functions/AVStartup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
+using static ArnoldVinkCode.AVAssembly;
 using static ArnoldVinkCode.AVInteropDll;
 using static ArnoldVinkCode.AVProcess;
 
@@ -10,22 +11,28 @@ namespace ArnoldVinkCode
     public partial class AVStartup
     {
         //Setup application defaults
-        public static bool SetupDefaults(ProcessPriority priorityLevel)
+        public static bool SetupDefaults(ProcessPriority priorityLevel, bool checkDoubleProcess)
         {
             try
             {
                 Debug.WriteLine("Setting application defaults.");
 
+                //Resolve missing assembly dll files
+                AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolveFile;
+
                 //Get current process information
                 ProcessMulti currentProcess = Get_ProcessMultiCurrent();
-                List<ProcessMulti> activeProcesses = Get_ProcessesMultiByName(currentProcess.ExeNameNoExt, true);
 
                 //Check if application is already running
-                if (activeProcesses.Count > 1)
+                if (checkDoubleProcess)
                 {
-                    Debug.WriteLine("Application " + currentProcess.ExeNameNoExt + " is already running, closing the process");
-                    Environment.Exit(0);
-                    return false;
+                    List<ProcessMulti> activeProcesses = Get_ProcessesMultiByName(currentProcess.ExeNameNoExt, true);
+                    if (activeProcesses.Count > 1)
+                    {
+                        Debug.WriteLine("Application " + currentProcess.ExeNameNoExt + " is already running, closing the process");
+                        Environment.Exit(0);
+                        return false;
+                    }
                 }
 
                 //Set working directory to executable directory
