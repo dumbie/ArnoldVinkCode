4d334865adb9246c81bc0061c25a2640901f2181
Arnold Vink <vink_arnold@hotmail.com>
2024-05-25 19:15:39 +0200

    - Added ListFindFirstIndex and ListReplaceFirstItem extensions.

--- Desktop/Extensions/AVExtensions.cs
+++ Desktop/Extensions/AVExtensions.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ArnoldVinkCode
@@ -7,30 +7,65 @@ namespace ArnoldVinkCode
     public static class AVExtensions
     {
         //List sort items
-        public static void ListSort<TSource, TKey>(this Collection<TSource> sourceList, Func<TSource, TKey> sortKey)
+        public static void ListSort<TSource, TKey>(this IList<TSource> sourceList, Func<TSource, TKey> sortCondition)
         {
             try
             {
                 sourceList.Clear();
-                foreach (TSource SortingItem in sourceList.OrderBy(sortKey).ToList())
+                foreach (TSource sortingItem in sourceList.OrderBy(sortCondition).ToList())
                 {
-                    sourceList.Add(SortingItem);
+                    sourceList.Add(sortingItem);
                 }
             }
             catch { }
         }
 
         //List remove all
-        public static void ListRemoveAll<TSource>(this Collection<TSource> sourceList, Func<TSource, bool> removeCondition)
+        public static void ListRemoveAll<TSource>(this IList<TSource> sourceList, Func<TSource, bool> removeCondition)
         {
             try
             {
-                foreach (TSource RemovalItem in sourceList.Where(removeCondition).ToList())
+                foreach (TSource removalItem in sourceList.Where(removeCondition).ToList())
                 {
-                    sourceList.Remove(RemovalItem);
+                    sourceList.Remove(removalItem);
                 }
             }
             catch { }
+        }
+
+        //List find first index
+        public static int ListFindFirstIndex<TSource>(this IList<TSource> sourceList, Func<TSource, bool> findCondition)
+        {
+            try
+            {
+                int listIndex = 0;
+                foreach (TSource element in sourceList)
+                {
+                    if (findCondition(element))
+                    {
+                        return listIndex;
+                    }
+                    listIndex++;
+                }
+            }
+            catch { }
+            return -1;
+        }
+
+        //List replace first item
+        public static bool ListReplaceFirstItem<TSource>(this IList<TSource> sourceList, Func<TSource, bool> replaceCondition, TSource newItem)
+        {
+            try
+            {
+                int listIndex = sourceList.ListFindFirstIndex(replaceCondition);
+                if (listIndex > -1)
+                {
+                    sourceList[listIndex] = newItem;
+                    return true;
+                }
+            }
+            catch { }
+            return false;
         }
     }
 }
