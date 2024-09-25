9f61c6675faf72df635222fb3c6046aecb3539c0
Arnold Vink <info@arnoldvink.com>
2024-09-25 12:00:14 +0200

    - Added textbox style that does not resize when text changed.

---
+++ Desktop/Styles/TextBoxNoResizeCode.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ArnoldVinkCode.Styles
+{
+    //Import:
+    //xmlns:AVStyles="clr-namespace:ArnoldVinkCode.Styles;assembly=ArnoldVinkCode"
+    //Usage:
+    //<AVStyles:TextBoxNoResize/>
+
+    public class TextBoxNoResize : TextBox
+    {
+        protected override Size MeasureOverride(Size constraint)
+        {
+            return Size.Empty;
+        }
+    }
+}
