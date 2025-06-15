f908705a066d8072bef16946d6a84b5cb0748b67
Arnold Vink <info@arnoldvink.com>
2025-06-15 17:13:14 +0200

    - Added AVSoundPlayer code.

---
+++ Desktop/Functions/AVSoundPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Media;
+
+namespace ArnoldVinkCode
+{
+    public partial class AVSoundPlayer
+    {
+        //Player Variables
+        private static MediaPlayer windowsMediaPlayer = new MediaPlayer();
+
+        //Play sound
+        public static void PlaySound(string soundFilePath, double soundVolume)
+        {
+            try
+            {
+                Uri soundFileUri = new Uri(soundFilePath, UriKind.RelativeOrAbsolute);
+                AVActions.DispatcherInvoke(delegate
+                {
+                    windowsMediaPlayer.Volume = soundVolume;
+                    windowsMediaPlayer.Open(soundFileUri);
+                    windowsMediaPlayer.Play();
+                });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed to play sound: " + soundFilePath + " / " + ex.Message);
+            }
+        }
+    }
+}
