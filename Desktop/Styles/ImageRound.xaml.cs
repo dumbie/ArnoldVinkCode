7fc21e7a29bd64216324037edcea2851e09fd5a9
Arnold Vink <info@arnoldvink.com>
2025-06-19 19:27:41 +0200

    - Added custom rounded image user control.

---
+++ Desktop/Styles/ImageRound.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ArnoldVinkCode.Styles
+{
+    public partial class ImageRound : UserControl
+    {
+        //Variables
+        public ImageSource Source
+        {
+            get
+            {
+                return image_Round.Source;
+            }
+            set
+            {
+                image_Round.Source = value;
+            }
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get
+            {
+                return border_Round.CornerRadius;
+            }
+            set
+            {
+                border_Round.CornerRadius = value;
+            }
+        }
+
+        //Initialize
+        public ImageRound()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch { }
+        }
+    }
+}
