c0640a00e042464b1006c14fef1ba666fd7f3cc7
Arnold Vink <info@arnoldvink.com>
2025-06-19 16:57:01 +0200

    - Added xml text reader that skips namespaces.

---
+++ Desktop/Functions/AVXml.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Xml;
+
+namespace ArnoldVinkCode
+{
+    public partial class AVXml
+    {
+        /// <summary>
+        /// Xml text reader that skips namespaces
+        /// Note: make sure to remove all namespaces from classes
+        /// </summary>
+        public class XmlTextReaderSkipNamespace : XmlTextReader
+        {
+            public XmlTextReaderSkipNamespace(Stream stream) : base(stream)
+            {
+                Namespaces = true;
+            }
+
+            public override string NamespaceURI => string.Empty;
+        }
+    }
+}
