ed4989fa777f9d2287e8db0b250737d7fb55e458
Arnold Vink <info@arnoldvink.com>
2025-07-03 16:04:11 +0200

    - Added code to sort observable collection listbox.

---
+++ Desktop/Functions/AVSortObservableCollection/Classes/SortFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArnoldVinkCode
+{
+    public partial class AVSortObservableCollection
+    {
+        public enum SortDirection
+        {
+            Default,
+            Ascending,
+            Descending
+        }
+
+        public class SortFunction<T>
+        {
+            public Func<T, object> Function { get; set; } = null;
+            public SortDirection Direction { get; set; } = SortDirection.Ascending;
+        }
+    }
+}
