32b8f18c4e51da313ff51a490bf758a0ae659ac6
Arnold Vink <info@arnoldvink.com>
2025-07-07 10:42:58 +0200

    - Added code to marshal object to bytes. - Added code to get dynamic type.

--- Desktop/Functions/AVClassConverters.cs
+++ Desktop/Functions/AVClassConverters.cs
@@ -3,12 +3,24 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Diagnostics;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
+using static ArnoldVinkCode.AVInteropDll;
 
 namespace ArnoldVinkCode
 {
     public partial class AVClassConverters
     {
+        //Get dynamic variable type
+        public static Type GetDynamicType(dynamic dynamicVariable)
+        {
+            try
+            {
+                return ((object)dynamicVariable).GetType();
+            }
+            catch { return null; }
+        }
+
         //Shallow clone object
         public static bool CloneObjectShallow<T>(T cloneObject, out T outObject)
         {
@@ -26,8 +38,56 @@ namespace ArnoldVinkCode
             }
         }
 
+        //Marshal object to bytes
+        public static byte[] MarshalObjectToBytes(object targetObject)
+        {
+            IntPtr intPtr = IntPtr.Zero;
+            try
+            {
+                int size = Marshal.SizeOf(targetObject);
+                intPtr = Marshal.AllocHGlobal(size);
+                Marshal.StructureToPtr(targetObject, intPtr, true);
+                byte[] byteArray = new byte[size];
+                Marshal.Copy(intPtr, byteArray, 0, size);
+                return byteArray;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed to MarshalObjectToBytes: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                SafeCloseMarshal(ref intPtr);
+            }
+        }
+
+        //Marshal bytes to object
+        public static bool MarshalBytesToObject<T>(byte[] targetBytes, out T outObject)
+        {
+            IntPtr intPtr = IntPtr.Zero;
+            try
+            {
+                int size = Marshal.SizeOf(default(T));
+                intPtr = Marshal.AllocHGlobal(size);
+                Marshal.Copy(targetBytes, 0, intPtr, size);
+                outObject = (T)Marshal.PtrToStructure(intPtr, typeof(T));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed to MarshalBytesToObject: " + ex.Message);
+                outObject = default(T);
+                return false;
+            }
+            finally
+            {
+                SafeCloseMarshal(ref intPtr);
+            }
+        }
+
         //Serialize object to bytes
-        public static byte[] SerializeObjectToBytes(object serializeObject)
+        public static byte[] SerializeObjectToBytes(object targetObject)
         {
             try
             {
@@ -36,7 +96,7 @@ namespace ArnoldVinkCode
                     //TypeNameHandling = TypeNameHandling.Objects
                 };
 
-                string jsonString = JsonConvert.SerializeObject(serializeObject, jsonSettings);
+                string jsonString = JsonConvert.SerializeObject(targetObject, jsonSettings);
                 return Encoding.UTF8.GetBytes(jsonString);
             }
             catch (Exception ex)
@@ -47,7 +107,7 @@ namespace ArnoldVinkCode
         }
 
         //Deserialize bytes to object
-        public static bool DeserializeBytesToObject<T>(byte[] bytesObject, out T outObject)
+        public static bool DeserializeBytesToObject<T>(byte[] targetBytes, out T outObject)
         {
             try
             {
@@ -56,7 +116,7 @@ namespace ArnoldVinkCode
                     //TypeNameHandling = TypeNameHandling.Objects
                 };
 
-                string jsonString = Encoding.UTF8.GetString(bytesObject);
+                string jsonString = Encoding.UTF8.GetString(targetBytes);
                 outObject = JsonConvert.DeserializeObject<T>(jsonString, jsonSettings);
                 return true;
             }
