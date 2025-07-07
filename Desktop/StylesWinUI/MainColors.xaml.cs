8eb35fb13ed3a5b3071a80b524275ea7e33e72ab
Arnold Vink <info@arnoldvink.com>
2025-07-07 12:44:31 +0200

    - Started porting WPF styles to WinUI.

---
+++ Desktop/StylesWinUI/MainColors.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace ArnoldVinkCode.Styles
+{
+    public partial class MainColors : ResourceDictionary
+    {
+        //Change application accent color
+        public static void ChangeApplicationAccentColor(string colorLightHex)
+        {
+            try
+            {
+                Debug.WriteLine("Changing the application accent color.");
+
+                //FixStyle
+                //SolidColorBrush targetSolidColorBrushLight = new BrushConverter().ConvertFrom(colorLightHex) as SolidColorBrush;
+                SolidColorBrush targetSolidColorBrushLight = new SolidColorBrush();
+
+                Application.Current.Resources["ApplicationAccentLightColor"] = targetSolidColorBrushLight.Color;
+                Application.Current.Resources["ApplicationAccentLightBrush"] = targetSolidColorBrushLight;
+                Debug.WriteLine("Accent light color: " + targetSolidColorBrushLight.Color);
+
+                SolidColorBrush targetSolidColorBrushDim = AVColors.AdjustColorBrightness(targetSolidColorBrushLight, 0.80);
+                Application.Current.Resources["ApplicationAccentDimColor"] = targetSolidColorBrushDim.Color;
+                Application.Current.Resources["ApplicationAccentDimBrush"] = targetSolidColorBrushDim;
+                Debug.WriteLine("Accent dim color: " + targetSolidColorBrushDim.Color);
+
+                SolidColorBrush targetSolidColorBrushDark = AVColors.AdjustColorBrightness(targetSolidColorBrushLight, 0.50);
+                Application.Current.Resources["ApplicationAccentDarkColor"] = targetSolidColorBrushDark.Color;
+                Application.Current.Resources["ApplicationAccentDarkBrush"] = targetSolidColorBrushDark;
+                Debug.WriteLine("Accent dark color: " + targetSolidColorBrushDark.Color);
+            }
+            catch { }
+        }
+    }
+}
