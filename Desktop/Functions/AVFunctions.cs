c6aa2c1e7eac96d2ca6e00d409a26087526cc647
Arnold Vink <info@arnoldvink.com>
2025-07-14 16:24:38 +0200

    - Moving WPF based style code to ArnoldVinkStyles project.

--- Desktop/Functions/AVFunctions.cs
+++ Desktop/Functions/AVFunctions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
@@ -7,11 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Security.Principal;
 using System.Text.RegularExpressions;
-using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Forms;
-using System.Windows.Media;
-using System.Windows.Threading;
 
 namespace ArnoldVinkCode
 {
@@ -443,161 +438,20 @@ namespace ArnoldVinkCode
             else { return (NowNumber > StartNumber) || (NowNumber < EndNumber); }
         }
 
-        //Reset dispatch timer tick estimate
-        public static void TimerReset(DispatcherTimer dispatchTimer)
-        {
-            try
-            {
-                if (dispatchTimer != null)
-                {
-                    dispatchTimer.Stop();
-                    dispatchTimer.Start();
-                }
-            }
-            catch { }
-        }
-
-        //Stop dispatch timer safely
-        public static void TimerStop(DispatcherTimer dispatchTimer)
-        {
-            try
-            {
-                if (dispatchTimer != null)
-                {
-                    dispatchTimer.Stop();
-                }
-            }
-            catch { }
-        }
-
-        //Renew dispatch timer
-        public static void TimerRenew(ref DispatcherTimer dispatchTimer)
-        {
-            try
-            {
-                if (dispatchTimer != null) { dispatchTimer.Stop(); }
-                dispatchTimer = new DispatcherTimer();
-            }
-            catch { }
-        }
-
-        //Find all visual children of object
-        public static List<T> FindVisualChildren<T>(DependencyObject objChild) where T : DependencyObject
-        {
-            List<T> childrenList = new List<T>();
-            try
-            {
-                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(objChild); i++)
-                {
-                    try
-                    {
-                        DependencyObject child = VisualTreeHelper.GetChild(objChild, i);
-                        if (child != null)
-                        {
-                            if (child is T)
-                            {
-                                childrenList.Add((T)child);
-                            }
-                            else
-                            {
-                                foreach (T childOfChild in FindVisualChildren<T>(child))
-                                {
-                                    childrenList.Add(childOfChild);
-                                }
-                            }
-                        }
-                    }
-                    catch { }
-                }
-            }
-            catch { }
-            return childrenList;
-        }
-
-        //Find the visual child of object
-        public static T FindVisualChild<T>(DependencyObject objChild) where T : DependencyObject
-        {
-            try
-            {
-                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(objChild); i++)
-                {
-                    try
-                    {
-                        DependencyObject child = VisualTreeHelper.GetChild(objChild, i);
-                        if (child != null)
-                        {
-                            if (child is T)
-                            {
-                                return (T)child;
-                            }
-                            else
-                            {
-                                T subChild = FindVisualChild<T>(child);
-                                if (subChild != null) { return subChild; }
-                            }
-                        }
-                    }
-                    catch { }
-                }
-            }
-            catch { }
-            return null;
-        }
-
-        //Find the visual parent of object
-        public static T FindVisualParent<T>(DependencyObject objChild) where T : DependencyObject
-        {
-            try
-            {
-                DependencyObject parentObject = VisualTreeHelper.GetParent(objChild);
-                if (parentObject == null) { return null; }
-
-                //Check if parent matches the type
-                T parent = parentObject as T;
-                if (parent != null)
-                {
-                    return parent;
-                }
-                else
-                {
-                    return FindVisualParent<T>(parentObject);
-                }
-            }
-            catch { }
-            return null;
-        }
-
-        //Check if an actual ListBoxItem is clicked
-        public static bool ListBoxItemClickCheck(DependencyObject dependencyObject)
-        {
-            try
-            {
-                if (FindVisualParent<ListBoxItem>(dependencyObject) == null)
-                {
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-            }
-            catch { return false; }
-        }
-
         //Get screen aspect ratio
-        public static string ScreenAspectRatio(int ScreenWidth, int ScreenHeight, bool AutoResolution)
+        public static string ScreenAspectRatio(int screenWidth, int screenHeight, bool autoResolution)
         {
             try
             {
-                if (AutoResolution)
+                if (autoResolution)
                 {
-                    ScreenWidth = Screen.PrimaryScreen.Bounds.Width;
-                    ScreenHeight = Screen.PrimaryScreen.Bounds.Height;
+                    screenWidth = Screen.PrimaryScreen.Bounds.Width;
+                    screenHeight = Screen.PrimaryScreen.Bounds.Height;
                 }
 
                 int Remainder = 0;
-                int Greatest = ScreenWidth;
-                int Common = ScreenHeight;
+                int Greatest = screenWidth;
+                int Common = screenHeight;
                 while (Common != 0)
                 {
                     Remainder = Greatest % Common;
@@ -607,16 +461,16 @@ namespace ArnoldVinkCode
 
                 if (Greatest != 0)
                 {
-                    return ScreenWidth / Greatest + ":" + ScreenHeight / Greatest;
+                    return screenWidth / Greatest + ":" + screenHeight / Greatest;
                 }
                 else
                 {
-                    return ScreenWidth + ":" + ScreenHeight;
+                    return screenWidth + ":" + screenHeight;
                 }
             }
             catch
             {
-                return ScreenWidth + ":" + ScreenHeight;
+                return screenWidth + ":" + screenHeight;
             }
         }
 
