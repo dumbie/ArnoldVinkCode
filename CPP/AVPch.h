aea9ff1a85fd4e3acf4dcee69e491dfe577c5a0b
Arnold Vink <info@arnoldvink.com>
2025-12-02 15:03:12 +0100

    - Added AVPch.h to easily include all files.

---
+++ CPP/AVPch.h
@@ -0,0 +1,23 @@
+//Arnold Vink Code
+#pragma once
+#include "AVAdmin.h"
+#include "AVDebug.h"
+#include "AVDownloader.h"
+#include "AVException.h"
+#include "AVFiles.h"
+#include "AVFinally.h"
+#include "AVHighResDelay.h"
+#include "AVPaths.h"
+#include "AVProcesses\ProcessCheck.h"
+#include "AVProcesses\ProcessWindow.h"
+#include "AVProcesses\ProcessDetail.h"
+#include "AVProcesses\ProcessGet.h"
+#include "AVProcesses\AVProcesses.h"
+#include "AVRegistry.h"
+#include "AVSettingsJson.h"
+#include "AVSettingsStartup.h"
+#include "AVString.h"
+#include "AVVersion.h"
+#include "nlohmann_json.hpp"
+using namespace ArnoldVinkCode;
+using namespace AVProcesses;
