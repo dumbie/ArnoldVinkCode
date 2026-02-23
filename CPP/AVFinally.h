1766060edcd3790ce58374ba618711f642234f64
Arnold Vink <info@arnoldvink.com>
2026-02-23 13:26:34 +0100

    - Started working on semi-smart delete/release that works for pointers and interfaces.

--- CPP/AVFinally.h
+++ CPP/AVFinally.h
@@ -1,25 +1,132 @@
 #pragma once
-#define AVFinally(callback) AVFinallyInternal x([&]{ callback });
-#define AVFinallySafe(callback) AVFinallyInternal x([&]{ try { callback } catch (...) {} });
+#include <unknwn.h>
+#include "AVDebug.h"
+#define AVFinally(callback) AVFinallyFunction x([&]{ callback });
+#define AVFinallySafe(callback) AVFinallyFunction x([&]{ try { callback } catch (...) {} });
 
+//Description: Runs code after going out of scope or loop
 //Usage example: AVFinally(void(););
 //Usage example: AVFinally({ void1(); void2(); });
-//Description: Runs code after a function returns
 //Note: Add in top of function before any other code is executed
 template <typename T>
-struct AVFinallyInternal
+class AVFinallyFunction
 {
+private:
 	T runFunction;
 	bool runTriggered = false;
 
-	AVFinallyInternal(T f) : runFunction{ f } {}
+public:
+	AVFinallyFunction(T f) : runFunction{ f } {}
 
-	~AVFinallyInternal()
+	~AVFinallyFunction()
 	{
 		if (!runTriggered)
 		{
 			runTriggered = true;
 			runFunction();
 		}
+	}
+};
+
+//Description: Automatically frees pointer after going out of scope or loop
+//Usage example: AVFinPtr<int*>();
+//Usage example: AVFinPtr(new int[1024]);
+//Usage example: avFinPtr.SetReleaser(0, [](auto, auto releasePointer) { releasePointer->Release(); });
+//Usage example: avFinPtr.SetReleaser(5, [](const int releaseItemCount, auto releasePointer)
+//{
+//	for (int i = 0; i < releaseItemCount; i++)
+//	{
+//		delete[](releasePointer[i].WCHAR);
+//	}
+//	delete[](releasePointer);
+//});
+//Note: Error C0000374 usually means you are using the wrong release method (new() = delete / new[] = delete[] / malloc = free / interface = Release)
+enum class AVFinPtrMethod
+{
+	DeleteSingle,
+	DeleteArray,
+	Release,
+	Free
+};
+
+template<typename T>
+class AVFinPtr
+{
+private:
+	T Pointer = nullptr;
+	int ReleaseItemCount = 0;
+	std::function<void(const int releaseItemCount, T& releasePointer)> ReleaseFunction = nullptr;
+	AVFinPtrMethod ReleaseMethod = AVFinPtrMethod::Free;
+
+	bool Release()
+	{
+		try
+		{
+			if (Pointer != nullptr)
+			{
+				if (ReleaseFunction != nullptr)
+				{
+					ReleaseFunction(ReleaseItemCount, Pointer);
+				}
+				else
+				{
+					if (ReleaseMethod == AVFinPtrMethod::DeleteSingle)
+					{
+						delete((void*)Pointer);
+					}
+					else if (ReleaseMethod == AVFinPtrMethod::DeleteArray)
+					{
+						delete[]((void*)Pointer);
+					}
+					else if (ReleaseMethod == AVFinPtrMethod::Release)
+					{
+						((IUnknown*)Pointer)->Release();
+					}
+					else
+					{
+						free((void*)Pointer);
+					}
+				}
+				Pointer = nullptr;
+				return true;
+			}
+		}
+		catch (...) {}
+		return false;
+	}
+
+public:
+	AVFinPtr() {};
+	AVFinPtr(T& setPointer)
+	{
+		Release();
+		Pointer = setPointer;
+	}
+
+	void SetPointer(T& setPointer)
+	{
+		Release();
+		Pointer = setPointer;
+	}
+
+	void SetReleaseMethod(AVFinPtrMethod setReleaseMethod)
+	{
+		ReleaseMethod = setReleaseMethod;
+	}
+
+	void SetReleaser(int setItemCount, std::function<void(const int releaseItemCount, T& releasePointer)> setFunction)
+	{
+		ReleaseItemCount = setItemCount;
+		ReleaseFunction = setFunction;
+	}
+
+	T& Get()
+	{
+		return Pointer;
+	}
+
+	~AVFinPtr()
+	{
+		Release();
 	}
 };
