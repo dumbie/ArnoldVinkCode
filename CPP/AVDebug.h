f9a7abb300aab6f630841b0d70f5ca07e8b5911c
Arnold Vink <info@arnoldvink.com>
2026-08-07 21:05:53 +0200

    - Added debug log file path.

--- CPP/AVDebug.h
+++ CPP/AVDebug.h
@@ -4,7 +4,8 @@
 #include <sstream>
 #include <fstream>
 #define AVDebugWriteLine(message) AVDebugWriteLineInternal() << message
-inline bool AVDebugWriteLineLogFile = false;
+inline bool AVDebugWriteLineLogFileEnabled = false;
+inline std::wstring AVDebugWriteLineLogFilePath = L"Debug.log";
 
 //Usage example: AVDebugWriteLine("Hello " << "World " << 1);
 //Usage example: AVDebugWriteLineInternal() << "Hello " << "World " << 1;
@@ -23,12 +24,24 @@ struct AVDebugWriteLineInternal
 	~AVDebugWriteLineInternal()
 	{
 		stringstream << "\n";
-		OutputDebugStringW(stringstream.str().c_str());
-		std::wcout << stringstream.str().c_str() << std::flush;
-		if (AVDebugWriteLineLogFile)
+
+		//Convert stream to text
+		std::wstring outputStringW = stringstream.str();
+		LPCWSTR outputStringC = outputStringW.c_str();
+
+		//Write text to console
+		DWORD writeConsoleResult;
+		WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), outputStringC, outputStringW.length(), &writeConsoleResult, NULL);
+
+		//Write text to output
+		OutputDebugStringW(outputStringC);
+
+		//Write text to log file
+		if (AVDebugWriteLineLogFileEnabled)
 		{
-			std::wofstream logstream("Debug.log", std::ios::app);
-			logstream << stringstream.str().c_str();
+			std::wofstream writeStream(AVDebugWriteLineLogFilePath, std::ios::app);
+			writeStream << outputStringC;
+			writeStream.close();
 		}
 	}
 };
